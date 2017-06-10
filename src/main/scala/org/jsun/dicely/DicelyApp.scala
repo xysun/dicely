f98053ba14ed613e3415e24ab3566c85b7a499fb
xysun <xiayunsun@gmail.com>
2017-06-10 17:36:22 +0700

    remove "return to stop"

--- src/main/scala/org/jsun/dicely/DicelyApp.scala
+++ src/main/scala/org/jsun/dicely/DicelyApp.scala
@@ -1,17 +1,29 @@
 package org.jsun.dicely
 
-import akka.http.scaladsl.server.{HttpApp, Route}
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.{Directives, HttpApp, Route}
+import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import org.jsun.dicely.routes.DicelyRoutesImpl
 
 import scala.util.Try
 
-object DicelyApp extends HttpApp with App with DicelyRoutesImpl {
+object DicelyApp extends Directives with DicelyRoutesImpl {
 
-  def routes: Route = shortenRoute ~ geturl
+  def main(args: Array[String]) {
 
-  val port = Try(args(0).toInt).getOrElse(ConfigFactory.load().getInt("port"))
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+    val port = Try(args(0).toInt).getOrElse(ConfigFactory.load().getInt("port"))
+    val bindingFuture = Http().bindAndHandle(routes, "localhost", port)
 
+  }
 
-  startServer("localhost", port)
+  // Here you can define all the different routes you want to have served by this web server
+  // Note that routes might be defined in separated traits like the current case
+  val routes = shortenRoute ~ geturl
+
 }
