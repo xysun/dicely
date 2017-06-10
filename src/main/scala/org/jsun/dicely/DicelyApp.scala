a5b14e03cc6e4f6f484a9b52afa052b095656b99
xysun <xiayunsun@gmail.com>
2017-06-10 17:23:26 +0700

    port on cmd line

--- src/main/scala/org/jsun/dicely/DicelyApp.scala
+++ src/main/scala/org/jsun/dicely/DicelyApp.scala
@@ -1,14 +1,17 @@
 package org.jsun.dicely
 
-import akka.http.scaladsl.server.{ HttpApp, Route }
+import akka.http.scaladsl.server.{HttpApp, Route}
 import com.typesafe.config.ConfigFactory
 import org.jsun.dicely.routes.DicelyRoutesImpl
+
+import scala.util.Try
 
 object DicelyApp extends HttpApp with App with DicelyRoutesImpl {
 
   def routes: Route = shortenRoute ~ geturl
 
-  val port = ConfigFactory.load().getInt("port")
+  val port = Try(args(0).toInt).getOrElse(ConfigFactory.load().getInt("port"))
+
 
   startServer("localhost", port)
 }
