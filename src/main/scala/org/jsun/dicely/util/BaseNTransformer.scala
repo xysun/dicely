2e8fdf0eb382cf9ef3c3a516e4c647eed6d12a08
xysun <xiayunsun@gmail.com>
2017-06-10 13:29:45 +0700

    random 2-char checksum to prevent direct enumeration of all values

--- src/main/scala/org/jsun/dicely/util/BaseNTransformer.scala
+++ src/main/scala/org/jsun/dicely/util/BaseNTransformer.scala
@@ -1,34 +1,46 @@
 package org.jsun.dicely.util
 
+import com.typesafe.config.ConfigFactory
+
+import scala.util.Random
+
 /**
  * Created by jsun on 6/8/2017 AD.
  */
 trait BaseNTransformer {
 
-  // todo: scramble alphabet, some kind of randomization for prefix (id mod?)
-
-  private val ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+  private val ALPHABET = ConfigFactory.load().getString("alphabet")
   private val BASE = ALPHABET.length
 
-  // todo: how to prevent ppl enumerate all integers
-  /* it might be useful to add a two character checksum to the url. That would prevent direct iteration of all the urls in your system. Something simple like f(checksum(id) % (62^2)) + f(id) = url_id”
-
-   */
-
-  def decode(str: String): Long = {
-    str.foldLeft(0L)((num, c) => num * BASE + ALPHABET.indexOf(c))
-  }
-
-  def encode(i: Long): String = {
+  def toBaseN(i: Long): String = {
     val str: StringBuilder = new StringBuilder
 
-    var num = i // todo: more scala
+    var num = i
     while (num > 0) {
       str.insert(0, ALPHABET.charAt((num % BASE).toInt))
       num /= BASE
     }
 
     str.mkString
+  }
+
+  def decode(str: String): Long = {
+    // first two char is checksum
+    require(str.length >= 3)
+    str.substring(2).foldLeft(0L)((num, c) => num * BASE + ALPHABET.indexOf(c))
+  }
+
+  def encode(i:Long):String = {
+
+    // with two character checksum to prevent direct iteration of all urls
+    // f(checksum(id) % (62^2)) + f(id) = url_id
+
+    val part2 = toBaseN(i)
+    val checksum = Random.nextInt(BASE * (BASE - 1)) + BASE // [BASE, BASE * BASE)
+    val part1 = toBaseN(checksum)
+    require(part1.length == 2)
+
+    s"$part1$part2"
   }
 
 }
--- src/test/scala/org/jsun/dicely/BaseNTransformerTest.scala
+++ src/test/scala/org/jsun/dicely/BaseNTransformerTest.scala
@@ -1,5 +1,6 @@
 package org.jsun.dicely
 
+import com.typesafe.config.ConfigFactory
 import org.jsun.dicely.util.BaseNTransformer
 import org.scalatest.FunSuite
 
@@ -12,10 +13,17 @@ class BaseNTransformerTest extends FunSuite
     with BaseNTransformer {
 
   test("should be bidirectional") {
-    for (i <- 1 to 100) {
+    for (i <- 1 to 1000) {
       val seed = Random.nextInt(Integer.MAX_VALUE) // make sure it's positive
       assert(decode(encode(seed)) == seed)
     }
+  }
+
+  test("checksum should always produce 2 character"){
+    val ALPHABET = ConfigFactory.load().getString("alphabet")
+    val BASE = ALPHABET.length
+    assert(toBaseN(BASE).length == 2)
+    assert(toBaseN(BASE * BASE - 1).length == 2)
   }
 
 }
