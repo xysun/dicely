7ecfa1a586486e0460bc66bfacc0f0c6f2a72d7c
xysun <xiayunsun@gmail.com>
2017-06-10 14:18:05 +0700

    redundant comment

--- src/main/scala/org/jsun/dicely/DicelyApp.scala
+++ src/main/scala/org/jsun/dicely/DicelyApp.scala
@@ -4,10 +4,6 @@ import akka.http.scaladsl.server.{ HttpApp, Route }
 import com.typesafe.config.ConfigFactory
 import org.jsun.dicely.routes.DicelyRoutesImpl
 
-/**
- * Server will be started calling `WebServerHttpApp.startServer("localhost", 8080)`
- * and it will be shutdown after pressing return.
- */
 object DicelyApp extends HttpApp with App with DicelyRoutesImpl {
 
   def routes: Route = shortenRoute ~ geturl
